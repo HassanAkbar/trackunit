25169f06d5358afb57a3643d6d740814af101374
Hassan <hassanakbar2@gmail.com>
2019-12-03 11:17:01 +0500

    trackunit specs added

---
+++ spec/trackunit/client_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe TrackUnit::Client do
+
+  let(:client_attrs) { TrackUnit::Client.new("ValidToken").class.default_options }
+  let(:trackunit_uri) { "https://api.trackunit.com/public" }
+ 
+  it "validates the addition of access token in TrackUnit client" do
+    expect(client_attrs[:base_uri]).to eq(trackunit_uri)
+    expect(client_attrs[:format]).to eq(:json)
+    expect(client_attrs[:default_params][:token]).to eq("ValidToken")
+  end
+end
---
+++ spec/trackunit/resource_spec.rb
@@ -0,0 +1,24 @@
+RSpec.describe TrackUnit::Resource do
+  let(:success_body_response) { File.open(File.expand_path("../../support/fixtures/units.json", __FILE__)).read }
+  before(:each) do
+    stub_request(:get, "https://api.trackunit.com/public/unit?token=ValidToken").
+    with(
+      headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Ruby'
+      }).
+    to_return(
+      status: 200,
+      body: success_body_response,
+      headers: {}
+    )
+  end
+
+  it "serializes response from API" do
+    units = TrackUnit::Client.new("ValidToken").units
+    binding.pry
+    serialized_response = Resource.serialize_response(success_body_response)
+    expect(units).to eq(serialized_response)
+  end
+end
